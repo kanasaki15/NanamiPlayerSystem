61b0ee4f5c70baff7104a9421ce825eb0d281802
tina-chan <kassi_0123@kassi-hp.info>
2022-08-22 05:36:54 +0900

    ver表記がnullになる対策

--- src/main/java/xyz/n7mn/dev/nanamiplayersystem/EventListener.java
+++ src/main/java/xyz/n7mn/dev/nanamiplayersystem/EventListener.java
@@ -1,6 +1,7 @@
 package xyz.n7mn.dev.nanamiplayersystem;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.viaversion.viaversion.api.Via;
@@ -47,36 +48,33 @@ public class EventListener implements Listener {
             OkHttpClient client = new OkHttpClient();
 
             Request request = new Request.Builder()
-                    .url("https://gitlab.bixilon.de/bixilon/minosoft/-/raw/master/src/main/resources/assets/minosoft/mapping/versions.json?inline=false")
+                    .url("https://raw.githubusercontent.com/hugmanrique/mc-versions/main/versions.json")
                     .build();
             Response response = client.newCall(request).execute();
             String json = response.body().string();
 
             JsonObject list = new Gson().fromJson(json, JsonObject.class);
-            int i = 0;
-            while (true){
+            JsonElement editions = list.get("editions");
+            JsonElement java = editions.getAsJsonObject().get("java");
+            JsonArray versions = java.getAsJsonObject().get("versions").getAsJsonArray();
 
-                if (i > 1000){
-                    return;
-                }
+            for (int i = 0; i < versions.size(); i++){
+                String version = versions.get(i).getAsJsonObject().get("name").getAsString();
 
-                String s = String.valueOf(i);
-                if (list.get(s) == null){
-                    i++;
-                    continue;
-                }
+                int protocolNumber = 0;
+                if (!versions.get(i).getAsJsonObject().get("protocolNumber").isJsonNull()){
+                    protocolNumber = versions.get(i).getAsJsonObject().get("protocolNumber").getAsInt();
+                };
 
-                JsonElement element = list.get(s);
-                JsonObject object = element.getAsJsonObject();
-                if (object.get("protocol_id") == null){
-                    protocolVersionList.put(i, object.get("name").getAsString());
-                } else {
-                    protocolVersionList.put(object.get("protocol_id").getAsInt(), object.get("name").getAsString());
+
+                if (protocolVersionList.get(protocolNumber) != null){
+                    version = version + "/" + protocolVersionList.get(protocolNumber);
                 }
-                i++;
+                protocolVersionList.put(protocolNumber, version);
             }
 
-        } catch (IOException e){
+
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
@@ -209,7 +207,12 @@ public class EventListener implements Listener {
         ViaAPI api = Via.getAPI();
         int protocolVersion = api.getPlayerVersion(e.getPlayer().getUniqueId());
 
-        String opMessage = ChatColor.translateAlternateColorCodes('&',plugin.getConfig().getString("Prefix")) + ChatColor.RESET+" " + e.getPlayer().getName() + "さんが入室しました。 (Ver: "+protocolVersionList.get(protocolVersion)+" 権限: " + permDisplayName + ")";
+        String ver = protocolVersionList.get(protocolVersion);
+        if (ver == null){
+            ver = "不明";
+        }
+
+        String opMessage = ChatColor.translateAlternateColorCodes('&',plugin.getConfig().getString("Prefix")) + ChatColor.RESET+" " + e.getPlayer().getName() + "さんが入室しました。 (Ver: "+ver+" 権限: " + permDisplayName + ")";
         plugin.getLogger().info(opMessage);
         if (plugin.getConfig().getBoolean("SendOPMessage")){
             new Thread(()->{
